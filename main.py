0aaec609c096518628aa25d6c718c208c8b0f1e8
zunayed <zunayed.arp@gmail.com>
2021-01-03 03:44:20 +0600

    question 1

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-string = "hello"
-print(string.upper())
+'''Exercise Question 1: Create a Vehicle class with max_speed and
+mileage instance attributes'''
+class Vehicles:
+ def __init__(self, max_speed, mileage):
+  self.max_speed = max_speed
+  self.mileage = mileage
+
+model_X = Vehicles(200,20)
+print(model_X.max_speed, model_X.mileage)
+
